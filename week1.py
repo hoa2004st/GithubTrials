3b0ac803e059974e41f232814859984c35940b52
unknown <hoa2004st.com>
2024-11-30 10:17:03 +0700

    ex2

--- week1.py
+++ week1.py
@@ -1,3 +1,22 @@
 ###ex1
 # a, b = [int(x) for x in input().split()]
 # print(a+b, a-b, a*b, a//b)
+
+###ex2
+# n = int(input())
+# int_list = [int(x) for x in input().split()]
+# _ = input()
+# text = input()
+# while text != '***':
+#     if text == 'find-max':
+#         print(max(int_list))
+#     elif text == 'find-min':
+#         print(min(int_list))
+#     elif text == 'sum':
+#         print(sum(int_list))
+#     else:
+#         _, x, y = text.split()
+#         x, y = int(x), int(y)
+#         print(max(int_list[x-1: y]))
+
+#     text = input()
