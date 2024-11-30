a36703d9097b8e1fd0a8c0e7416589669c7a7025
unknown <hoa2004st.com>
2024-11-30 10:19:44 +0700

    ex6-8

--- week1.py
+++ week1.py
@@ -43,3 +43,34 @@
 #         break
 
 # print(text.upper())
+###ex6
+# n, k = [int(x) for x in input().split()]
+# ints = [int(x) for x in input().split()]
+# weight = sum(ints[:k])
+# if weight %2 == 0:
+#     Q = 1
+# else:
+#     Q = 0
+# for i in range(1, n-k+1):
+#     weight += ints[i-1+k] - ints[i-1]
+#     if weight %2 == 0:
+#         Q += 1
+# print(Q)
+
+###ex7
+# count = 0
+# while True:
+#     try:
+#         count += len(input().split())
+#     except:
+#         break
+
+# print(count)
+
+###ex8
+# P1 = input()
+# P2 = input()
+# T = input()
+# while P1 in T:
+#     T = T.replace(P1, P2)
+# print(T)
