68b0090fd4d921cfb6bd121d1842c7f9b7ba6368
hoa.pm225495 <hoa2004st@gmail.com>
2024-12-01 09:50:24 +0700

    Max Flow

--- week6.py
+++ week6.py
@@ -32,3 +32,60 @@
 # result = FW(n, edges)
 # for row in result:
 #     print(*row)
+
+##### Max Flow
+# from collections import deque
+# def BFS(capacity, s, t, prev):
+#     n = len(capacity)
+#     visited = [False] * n
+#     queue = deque([s])
+#     visited[s] = True
+
+#     while queue:
+#         u = queue.popleft()
+#         for v in range(n):
+#             if not visited[v] and capacity[u][v] > 0:
+#                 queue.append(v)
+#                 visited[v] = True
+#                 prev[v] = u
+#                 if v == t:
+#                     return True
+#     return False
+
+# def MF(n, capacity, s, t):
+#     prev = [-1] * n
+#     max_flow = 0
+
+#     while BFS(capacity, s, t, prev):
+#         flow = float('inf')
+#         u = t
+#         while u != s:
+#             flow = min(flow, capacity[prev[u]][u])
+#             u = prev[u]
+
+#         v = t
+#         while v != s:
+#             u = prev[v]
+#             capacity[u][v] -= flow
+#             capacity[v][u] += flow
+#             v = u
+
+#         max_flow += flow
+
+#     return max_flow
+
+# n, m = map(int, input().split())
+# s, t = map(int, input().split())
+# s -= 1
+# t -= 1
+
+# capacity = [[0] * n for _ in range(n)]
+
+# for _ in range(m):
+#     u, v, c = map(int, input().split())
+#     u -= 1
+#     v -= 1
+#     capacity[u][v] += c
+
+# result = MF(n, capacity, s, t)
+# print(result)
